7ff99eb0c743eb3ba960a86f1f472b2f3aa2eac3
rizal14 <rizalfantofani@rocketmail.com>
2014-02-12 09:32:30 +0700

    commit kedua

--- insert.php
+++ insert.php
@@ -1,6 +1,8 @@
 <?php
 include "konek.php"; 
-$query = ("INSERT INTO deployment(db_id,dbversi_id,host_id,dbname_target,stagging) VALUES ('4','1288127179','7','contoh')");
-$hasil = mysql_query($query);
+$query = ("INSERT INTO `deployment`(`db_id`, `dbversi_id`, `host_id`, `dbname_target`, `staging`, `is_deploy`, `last_modif`, `username_modif`) VALUES (2,1388127176,7,2,null,0,2014-02-14,1)");
+echo $query;
+mysql_query($query) or die(mysql_error()); 
+//$hasil = mysql_query($query);
 
 ?>  
